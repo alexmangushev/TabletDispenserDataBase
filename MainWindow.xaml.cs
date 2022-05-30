13ffe49717861ce20eab5eedc0c0e29a65f11d7a
alexmangushev <mangushev2001@yandex.ru>
2022-05-30 13:20:48 +0300

    add some asks for db

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,59 @@ namespace TabletDispenserAdminPanel
     /// </summary>
     public partial class MainWindow : Window
     {
+        DBConnect DataBase;
         public MainWindow()
         {
             InitializeComponent();
+            DataBase = new DBConnect();
+            DataBase.OpenConnection();
+            DataBase.CloseConnection();
+        }
+
+
+
+        private void ChangeTableComboBox(object sender, SelectionChangedEventArgs e)
+        {
+            // = SelectTableComboBox.Text;
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem item = (ComboBoxItem)comboBox.SelectedItem;
+            TextBlock textComboBox = (TextBlock)item.Content;
+            string table = textComboBox.Text;
+            
+            switch(table)
+            {
+                case "Пациенты":
+                    MessageBox.Show(table);
+                    string columns = "patient_first_name, patient_patronymic, patient_last_name";
+                    string where = "id_patients = 5";
+                    int column_count = 3;
+                    List<string>[] data = DataBase.Select("patients", columns, column_count, where);
+                    //DBGrid
+                    break;
+            }
+
+            /*MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;" +
+                "pwd=!Mango_567;database=tablet_dispenser";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }*/
+        }
+
+        private void DataWindow_Closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            System.Environment.Exit(0);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-TabletDispenserAdminPanel
+# Admin panel for database for tablet dispenser
+
+This aplication allows to connect to database and do some CRUD and business tasks.
